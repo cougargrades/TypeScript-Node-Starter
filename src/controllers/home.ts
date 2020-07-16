94449a00a57f9021d1f48b60c8fb7849a55c5f1f
Austin Jackson <themacphage@gmail.com>
2020-07-15 22:29:53 -0500

    Sometimes there's still trash in them. Standing around. Smelling, and being useless.

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,11 +1,9 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 /**
  * GET /
  * Home page.
  */
 export const index = (req: Request, res: Response) => {
-    res.render("home", {
-        title: "Home"
-    });
+  res.send('Hello, World!');
 };
